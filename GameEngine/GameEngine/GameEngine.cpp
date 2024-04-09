67e613489c1650fcfb344b0f6ae0a12175da56da
RiccardoZappa <rzappa00@gmail.com>
2024-04-09 23:51:56 +0200

    removing GameEngine Header

--- GameEngine/GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine/GameEngine.cpp
@@ -1,12 +1,7 @@
 ﻿// GameEngine.cpp : Defines the entry point for the application.
 //
 
-#include "GameEngine.h"
-
-using namespace std;
-
 int main()
 {
-	cout << "Hello CMake." << endl;
 	return 0;
 }
