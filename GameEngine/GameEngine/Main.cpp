66a3f615d853fea960ad0de52720e2d997c51cab
RiccardoZappa <rzappa00@gmail.com>
2024-04-21 23:49:15 +0200

    completed the first functionalities of the window

--- GameEngine/GameEngine/Main.cpp
+++ GameEngine/GameEngine/Main.cpp
@@ -1,18 +1,22 @@
 ﻿// Main.cpp : Defines the entry point for the application.
 #include <Windows.h>
 
-#define MAX_NAME_STRING 256;
-#define HInstance() GetModuleHandle(NULL);
+#define MAX_NAME_STRING 256
+#define HInstance() GetModuleHandle(NULL)
 
-WCHAR WindowClass [MAX_NAME_STRING];
+WCHAR WindowClass[MAX_NAME_STRING];
+WCHAR WindowTitle[MAX_NAME_STRING];
+INT WindowWidth = 1366;
+INT WindowHeight = 768;
 
 int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 {
 
 	/* Initialize global variables*/
-	wcscpy_s(WindowClass, TEXT("TutorialWindow"));
+	wcscpy_s(WindowClass, MAX_NAME_STRING, L"TutorialWindow");
+	wcscpy_s(WindowTitle, MAX_NAME_STRING, L"My First GameEngine Window");
 
-	/*Create a window*/
+	/*Create  window class*/
 	WNDCLASSEX WndCex;
 	WndCex.cbSize = sizeof(WNDCLASSEX);
 	WndCex.style = CS_HREDRAW | CS_VREDRAW;
@@ -25,7 +29,7 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 	WndCex.hIcon = LoadIcon(0, IDI_APPLICATION);
 	WndCex.hIconSm = LoadIcon(0, IDI_APPLICATION);
 
-	WndCex.lpszMenuName = WindowClass;
+	WndCex.lpszMenuName = (LPCSTR)WindowClass;
 
 	WndCex.lpszMenuName = nullptr;
 	WndCex.hInstance = HInstance();
@@ -33,6 +37,32 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 	WndCex.lpfnWndProc = DefWindowProc;
 	RegisterClassEx(&WndCex);
 
+	/*Create and display our window */
+
+	HWND hwn = CreateWindow((LPCSTR)WindowClass, (LPCSTR)WindowTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, WindowWidth, WindowHeight, nullptr, nullptr, HInstance(), nullptr);
+	if(!hwn)
+	{
+		MessageBox(0, "Failed to create the window!", 0, 0);
+		return 0;
+	}
+	ShowWindow(hwn, SW_SHOW);
+
+	/* Listen for message event */
+
+	MSG msg{ 0 };
+	while (msg.message != WM_QUIT)
+	{
+		if(PeekMessage(&msg,0,0,0,PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 	return 0;
 }
 
+int main()
+{
+	return WinMain(nullptr,nullptr,nullptr,1);
+}
