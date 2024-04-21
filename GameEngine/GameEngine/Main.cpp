72ce07407144d76c78b1be9de078dc30f0d5efff
RiccardoZappa <rzappa00@gmail.com>
2024-04-21 23:06:17 +0200

    Finishing setting up my windows

--- GameEngine/GameEngine/Main.cpp
+++ GameEngine/GameEngine/Main.cpp
@@ -1,16 +1,37 @@
 ﻿// Main.cpp : Defines the entry point for the application.
 #include <Windows.h>
 
-int CALLBACK WinMain(HINSTANCE hIstance, HINSTANCE, LPSTR lpCmdLine, INT nCmdshow)
+#define MAX_NAME_STRING 256;
+#define HInstance() GetModuleHandle(NULL);
+
+WCHAR WindowClass [MAX_NAME_STRING];
+
+int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 {
 
-	/*Create a window*/
-	WNDCLASSEX Wnd;
-	Wnd.cbSize = sizeof(WNDCLASSEX);
-	Wnd.style = CS_HREDRAW | CS_VREDRAW;
-	Wnd.cbClsExtra = 0;
-	Wnd.cbWndExtra = 0;
+	/* Initialize global variables*/
+	wcscpy_s(WindowClass, TEXT("TutorialWindow"));
 
+	/*Create a window*/
+	WNDCLASSEX WndCex;
+	WndCex.cbSize = sizeof(WNDCLASSEX);
+	WndCex.style = CS_HREDRAW | CS_VREDRAW;
+	WndCex.cbClsExtra = 0;
+	WndCex.cbWndExtra = 0;
+	   
+	WndCex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	WndCex.hbrBackground = static_cast<HBRUSH>(GetStockObject(NULL_BRUSH));
+
+	WndCex.hIcon = LoadIcon(0, IDI_APPLICATION);
+	WndCex.hIconSm = LoadIcon(0, IDI_APPLICATION);
+
+	WndCex.lpszMenuName = WindowClass;
+
+	WndCex.lpszMenuName = nullptr;
+	WndCex.hInstance = HInstance();
+
+	WndCex.lpfnWndProc = DefWindowProc;
+	RegisterClassEx(&WndCex);
 
 	return 0;
 }
