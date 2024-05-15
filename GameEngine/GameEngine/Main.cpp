0be4a40ec2da0489417fc08826c34e534eefe6d5
RiccardoZappa <rzappa00@gmail.com>
2024-05-15 13:08:48 +0200

    added code to start utilizing the fragment and vertex shaders

--- GameEngine/GameEngine/Main.cpp
+++ GameEngine/GameEngine/Main.cpp
@@ -7,16 +7,15 @@
 #endif
 
 #include <iostream>
-// GLEW
-//#define GLEW_STATIC
-//#include <glew/glew.h>
-#include "../include/glad/glad.h"
+
+#include <glm/glm.hpp>
+#include <glad/glad.h>
+
 
 //GLM
-#include "../include/glm/glm.hpp"
-#include <glm/gtc/matrix_transform.hpp> 
+
 // GLFW
-#include "../include/glfw/glfw3.h"
+#include <glfw/glfw3.h>
 #include "Utils/model.h"
 #include "Utils/shaders.h"
 
@@ -35,19 +34,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 // Window dimensions
 const GLuint WIDTH = 1200, HEIGHT = 900;
 
-// Shaders
-const GLchar* vertexShaderSource = "#version 410 core\n"
-"layout (location = 0) in vec3 position;\n"
-"void main()\n"
-"{\n"
-"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-"}\0";
-const GLchar* fragmentShaderSource = "#version 410 core\n"
-"out vec4 color;\n"
-"void main()\n"
-"{\n"
-"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
 
 // The MAIN function, from here we start the application and run the game loop
 int main()
@@ -96,61 +82,22 @@ int main()
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
 
-    //set bufferSize callback
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
     // Build and compile our shader program
-    // we create and compile shaders (code of Shader class is in include/utils/shader.h)
+   // we create and compile shaders (code of Shader class is in include/utils/shader.h)
     Shader shader("00_basic.vert", "00_basic.frag");
 
     // we load the model(s) (code of Model class is in include/utils/model.h)
-    Model cubeModel("../../models/cube.obj");
-    Model sphereModel("../../models/sphere.obj");
-    Model bunnyModel("../../models/bunny_lp.obj");
+    Model cubeModel("../models/cube.obj");
+    Model sphereModel("../models/sphere.obj");
+    Model bunnyModel("../models/bunny_lp.obj");
 
     // We "install" the Shader Program as part of the current rendering process
     // with only one Shader Program, we can do this call now, and not inside the main loop:
     // we will use this Shader Program for everything is rendered after this call
     shader.Use();   // <-- Don't forget this one!
 
-
-    // Vertex shader
-    GLint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // Check for compile time errors
-    GLint success;
-    GLchar infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // Fragment shader
-    GLint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // Check for compile time errors
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // Link shaders
-    GLint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // Check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    //set bufferSize callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 
     // Set up vertex data (and buffer(s)) and attribute pointers
@@ -175,31 +122,6 @@ int main()
         1, 2, 3   // Second Triangle
     };
 
-    //rotate the vertices
-    float angle = glm::radians(45.0f);
-    glm::vec3 axis{ 1.0,0.0,0.0 };
-    glm::mat4  rotationMatrix = glm::rotate(glm::mat4(1.0f), angle, axis);
-    
-    GLuint VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
-
-    glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
-
 
     // Uncommenting this call will result in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -210,25 +132,11 @@ int main()
         // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
         glfwPollEvents();
 
-        // Render
-        // Clear the colorbuffer
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
 
-        // Draw our first triangle
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
 
         // Swap the screen buffers
         glfwSwapBuffers(window);
     }
-    // Properly de-allocate all resources once they've outlived their purpose
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
     // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
     return 0;
